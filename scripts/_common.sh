bba812fbb5b698ba857089a4eba65a639191c670
ericgaspar <junk.eg@free.fr>
2021-08-13 14:48:24 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="7.3"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-curl"
